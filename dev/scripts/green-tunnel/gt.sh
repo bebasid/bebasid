5b5056a0a78281bca72ee0e27da2ce490a733ef1
Palguno Wicaksono <palgunowicaksono@gmail.com>
2020-03-28 09:17:54 +0700

    tmux is not termux!
    1. Ganti screen ke tmux, lebih cakep tmux ternyata
    2. Hapus fitur lihat log (di tmux udah otomatis lihat log)
    3. Add fitur update, kasihan tester harus wgat wget ato ga curl 
    4. Ganti cara cek command ada atau tidak (nyesuain distro lain)
    5. Pastinya juga ikutan nambah bug....

--- dev/scripts/green-tunnel/gt.sh
+++ dev/scripts/green-tunnel/gt.sh
@@ -49,25 +49,21 @@ install_all(){
       sudo bash nodesource_setup.sh
       sudo rm -rf nodesource_setup.sh
       sudo apt update
-      sudo apt -y install nodejs screen
+      sudo apt -y install nodejs tmux
       sudo npm i -g green-tunnel
         ;;
-      # ARCH DERIVATIVE
+      # DINASTI ARCH, HOHOHO
       "arch")
       sudo pacman -Syy
-      sudo pacman -S npm screen
+      sudo pacman -S npm tmux
       sudo npm i -g green-tunnel
       ;;
   esac
 }
 
 open_gt(){
-	random=$(shuf -i 6000-8000 -n 1)
-	screen -d -m -S '6f4f9a675d5c67aa28350b0276bf911d' gt --ip 127.0.0.1 --port $random --dns-server https://doh.dnslify.com/dns-query --system-proxy false --silent true -v 'green-tunnel:*' proc
-	killall chrome
-	text="Tunggu sebentar, sedang membuka Google Chrome"
-	rand=0.0001
-	load
+  random=$(shuf -i 6000-8000 -n 1)
+  killall chrome
   cek_distro
   case $OS in
     "debian" )
@@ -77,17 +73,29 @@ open_gt(){
       stable="-stable"
       ;;
   esac
-	screen -d -m -S '554838a8451ac36cb977e719e9d6623c' google-chrome$stable netflix.com --proxy-server=127.0.0.1:$random
-  echo "Green Tunnel = 127.0.0.1:$random"
+  # TMUX IS NOT TERMUX.. OKAY
+  tmux new-session -d -s 6f4f9a675d5c67aa28350b0276bf911d -x 252 -y 29
+  tmux split-window -v
+  tmux split-window -h
+  tmux send-keys -t 1 "gt --ip 127.0.0.1 --port $random --dns-server https://doh.dnslify.com/dns-query --system-proxy false --silent true -v 'green-tunnel:*'" Enter
+  text="Tunggu sebentar, sedang membuka Google Chrome"
+  rand=0.1
+  load
+  tmux send-keys -t 2 "google-chrome$stable netflix.com --proxy-server=127.0.0.1:$random" Enter
+  tmux send-keys -t 0 "bebasid-gt 6f4f9a675d5c67aa28350b0276bf911d $random" Enter
+  tmux select-pane -t 0
+  tmux a
 }
 
 case $1 in
   start )
-    if ! [ -e /usr/bin/gt ]; then
+    if ! command -v gt > /dev/null; then
       echo "Install Dulu Green-Tunnelnya"
+      exit 1
     else
-      if ! [ -e /usr/bin/screen ] ; then
-        echo "Install Dulu Screennya"
+      if ! command -v tmux > /dev/null ; then
+        echo "Install Dulu Tmuxnya"
+        exit 1
       else
       open_gt
       fi
@@ -96,11 +104,40 @@ case $1 in
   install )
     install_all
     ;;
-  gete )
-    screen -r '6f4f9a675d5c67aa28350b0276bf911d'
+  --help )
+    echo "BEBASID + Green Tunnel"
+    echo "Cara penggunaan:"
+    echo "bebasid-gt [command]"
+    echo ""
+    echo "List command:"
+    echo "install   : Menginstall bahan-bahan yang diperlukan (NPM, GT, TMUX)"
+    echo "--help    : Menampilkan bantuan"
+    echo "start     : Memulai sesi Green Tunnel + Google Chrome"
+    echo "stop      : Mematikan sesi"
+    echo "update    : Memperbarui script bash BEBASID + Green Tunnel"
+    echo ""
+    echo "Aplikasi ini masih dalam tahap ujicoba alpha, sehingga mungkin banyak bug"
     ;;
   stop )
-    screen -S 6f4f9a675d5c67aa28350b0276bf911d -X quit
-    screen -S 554838a8451ac36cb977e719e9d6623c -X quit
+    tmux kill-session -t 6f4f9a675d5c67aa28350b0276bf911d
+    ;;
+  update )
+    if sudo curl -o /usr/local/bin/bebasid-gt https://raw.githubusercontent.com/bebasid/bebasid/master/dev/scripts/green-tunnel/gt.sh --progress-bar; then
+      echo ""
+      echo "Berhasil mengunduh script aplikasi BEBASID"
+      echo "Mengecek aplikasi"
+      sudo bebasid --about
+    else
+      echo "Gagal Update"
+    fi
+    ;;
+  6f4f9a675d5c67aa28350b0276bf911d )
+    reset
+    echo "Green Tunnel berhasil dibuka (127.0.0.1:$2)"
+    echo "Terminal ini jangan ditutup selama masih streaming"
+    echo "Untuk menonaktifkan, silahkan ketik perintah bebasid-gt stop"
+    ;;
+  *)
+    echo "Perintah tidak dikenali, ketik bebasid-gt --help untuk bantuan"
     ;;
 esac
