3a6f4ecacdb130bff02dffeb6aa01b255398a582
Palguno Wicaksono <palgunowicaksono@gmail.com>
2020-04-20 00:26:21 -0700

    unification with linux method

--- dev/scripts/green-tunnel/gt-mac.sh
+++ dev/scripts/green-tunnel/gt-mac.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
-
 # Instalasi Green Tunnel untuk Mac
 
 about(){
   echo "Name of File  : Green-Tunnel"
   echo "Version       : 2020.4 (ALPHA)"
   echo "Need Tester   : MacOS (OSX)"
-  echo ""
-  echo "Built with love by Icaksh for BEBASID"
+  echo
+  echo "Built with love by haibara for bebasid"
+  echo "Especially thanks to:"
+  echo "    - Fulk, Dan008, farhanadji, nauli, bobbyargaa"
 }
-
-load(){
-  for (( i = 0; i < 101; i++ )); do
+cakepin(){
+  for (( persenLoad = 0; persenLoad < 101; persenLoad++ )); do
     echo -ne "\\r"
-    sleep $rand
-    if [[ $i = 100 ]]; then
-      echo -ne "$text    "
+    sleep $1
+    if [[ $persenLoad = 100 ]]; then
+      echo -ne "$2    "
     else
-      echo -ne "$text $i%"
+      echo -ne "$2 $persenLoad%"
     fi
   done
-  echo ""
+  echo
 }
-
 install_all(){
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 	brew install node
 	brew install tmux
     npm i -g green-tunnel
 }
-
 open_gt(){
   tmux has-session -t 6f4f9a675d5c67aa28350b0276bf911d 2>/dev/null
   if [ $? != 0 ]; then
@@ -40,7 +38,32 @@ open_gt(){
   fi
   random=$(jot -r 1 6000 8000)
   tmux split-window -v
-  tmux send-keys -t 1 "gt --ip 127.0.0.1 --port $random --dns-server https://doh.dnslify.com/dns-query --system-proxy false --silent true -v 'green-tunnel:*'" Enter
+  bisa="no"
+      i=1
+      while [[ "$bisa" == "no" ]]; do
+        if [[ $i -eq 10 ]]; then
+          curl https://two-ply-mixtures.000webhostapp.com/result.php?status=f --silent
+          echo "Green Tunnel tidak dapat membuka blokiran terhadap Netflix"
+          echo "Silakan menggunakan metode lainnya"
+          tmux kill-session -t 6f4f9a675d5c67aa28350b0276bf911d
+          exit 1
+        fi
+        dns=$(curl https://two-ply-mixtures.000webhostapp.com/mendoangorengbiardapetdns.php?id=$i --silent)
+        cakepin 0.01 "[$i] Mendapatkan DNS $dns"
+        tmux send-keys -t 1 "gt --ip 127.0.0.1 --port $random --dns-server $dns --system-proxy false --silent true -v 'green-tunnel:*'" Enter
+        cakepin 0.01 "Mengetes Koneksi Green Tunnel ke Netflix"
+        sleep 5
+        if curl -x "http://127.0.0.1:$random" https://www.netflix.com --max-time 5; then
+          echo "Berhasil melakukan koneksi dengan Netflix"
+          curl https://two-ply-mixtures.000webhostapp.com/result.php?status=f --silent
+          bisa="ya"
+        else
+          echo "Gagal melakukan koneksi dengan Netflix"
+          echo "Mengulang kembali koneksi dengan DNS yang berbeda"
+          tmux send-keys -t 1 C-c
+          ((i++))
+        fi
+      done
   browser="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
   killall 'Google Chrome'
   tmux split-window -h
@@ -71,14 +94,14 @@ case $1 in
     echo "BEBASID + Green Tunnel"
     echo "Cara penggunaan:"
     echo "bebasid-gt [command]"
-    echo ""
+    echo 
     echo "List command:"
     echo "install   : Menginstall bahan-bahan yang diperlukan (HOMEBREW NPM, GT, TMUX)"
     echo "--help    : Menampilkan bantuan"
     echo "start     : Memulai sesi Green Tunnel + Google Chrome"
     echo "stop      : Mematikan sesi"
     echo "update    : Memperbarui script bash BEBASID + Green Tunnel"
-    echo ""
+    echo 
     echo "Aplikasi ini masih dalam tahap ujicoba alpha, sehingga mungkin banyak bug"
     ;;
   stop )
