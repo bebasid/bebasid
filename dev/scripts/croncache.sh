7d405f0388910dfccb2aa0b8e2d097220473aa71
Syafa Adena <gvoze32@gmail.com>
2022-08-04 15:07:17 +0700

    Add versioning system

--- dev/scripts/croncache.sh
+++ dev/scripts/croncache.sh
@@ -11,11 +11,13 @@ else
     cp /home/bebasid/public_html/hostsdefault /home/bebasid/public_html/hosts
     
     sekarang=$(date)
+    versi=`date +%M%H%m%d%Y`
     total=$(wc -l < /home/bebasid/public_html/hosts)
     
-    sed -i "5i \ " /home/bebasid/public_html/hosts
-    sed -i "6i # Tanggal: $sekarang" /home/bebasid/public_html/hosts
+    sed -i "4i \ " /home/bebasid/public_html/hosts
+    sed -i "5i # Versi: $versi" /home/bebasid/public_html/hosts
+    sed -i "6i # Tanggal diperbarui: $sekarang" /home/bebasid/public_html/hosts
     sed -i "7i # Total domain: ~$total" /home/bebasid/public_html/hosts
     sed -i "8i # Mengambil data terbaru dari: https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts" /home/bebasid/public_html/hosts
-    sed -i "9i # Data diambil otomatis tiap 1 hari sekali" /home/bebasid/public_html/hosts
+    sed -i "9i # Data diambil otomatis 4x sehari tiap 6 jam sekali" /home/bebasid/public_html/hosts
 fi
