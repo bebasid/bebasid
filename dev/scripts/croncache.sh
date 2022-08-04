5c317b1e93dc93d17d5064b259ef243908e770c7
Syafa Adena <gvoze32@gmail.com>
2022-08-04 14:46:07 +0700

    Update croncache.sh

--- dev/scripts/croncache.sh
+++ dev/scripts/croncache.sh
@@ -1,8 +1,21 @@
-curl -o /home/bebasidc/public_html/hosts https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts
-sekarang=$(date)
-total=$(wc -l < /home/bebasidc/public_html/hosts)
-sed -i "5i \ " /home/bebasidc/public_html/hosts
-sed -i "6i # Tanggal: $sekarang" /home/bebasidc/public_html/hosts
-sed -i "7i # Total domain: $total" /home/bebasidc/public_html/hosts
-sed -i "8i # Mengambil data terbaru dari: https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts" /home/bebasidc/public_html/hosts
-sed -i "9i # Data diambil otomatis tiap 1 hari sekali" /home/bebasidc/public_html/hosts
+#!/bin/bash
+wget https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts -O /home/bebasid/public_html/hostsdefault
+
+cmp /home/bebasid/public_html/hostsdefault /home/bebasid/public_html/hostscheck
+status=$?
+
+if [[ $status = 0 ]]; then
+    echo "Tidak ada pembaruan"
+else
+    cp /home/bebasid/public_html/hostsdefault /home/bebasid/public_html/hostscheck
+    cp /home/bebasid/public_html/hostsdefault /home/bebasid/public_html/hosts
+    
+    sekarang=$(date)
+    total=$(wc -l < /home/bebasid/public_html/hosts)
+    
+    sed -i "5i \ " /home/bebasid/public_html/hosts
+    sed -i "6i # Tanggal: $sekarang" /home/bebasid/public_html/hosts
+    sed -i "7i # Total domain: ~$total" /home/bebasid/public_html/hosts
+    sed -i "8i # Mengambil data terbaru dari: https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts" /home/bebasid/public_html/hosts
+    sed -i "9i # Data diambil otomatis tiap 1 hari sekali" /home/bebasid/public_html/hosts
+fi
