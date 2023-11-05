f2a1d9238cbcc1b986d4d5112b0f9b2342ff2210
Syafa Adena <gvoze32@gmail.com>
2023-11-06 00:59:00 +0700

    Update croncache.sh

--- dev/scripts/croncache.sh
+++ dev/scripts/croncache.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts -O /home/bebasid/public_html/hostsdefault
+path=$(pwd)
+hostsdefault_path="$path/public_html/hostsdefault"
 
-cmp /home/bebasid/public_html/hostsdefault /home/bebasid/public_html/hostscheck
+wget https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts -O "$hostsdefault_path"
+
+cmp "$hostsdefault_path" "$path/public_html/hostscheck"
 status=$?
 
 if [[ $status = 0 ]]; then
     echo "Tidak ada pembaruan"
 else
-    cp /home/bebasid/public_html/hostsdefault /home/bebasid/public_html/hostscheck
-    cp /home/bebasid/public_html/hostsdefault /home/bebasid/public_html/hosts
+    cp "$hostsdefault_path" "$path/public_html/hostscheck"
+    cp "$hostsdefault_path" "$path/public_html/hosts"
     
     sekarang=$(date)
     versi=`date +%M%H%m%d%Y`
-    total=$(wc -l < /home/bebasid/public_html/hosts)
+    total=$(wc -l < "$path/public_html/hosts")
     
-    sed -i "4i \ " /home/bebasid/public_html/hosts
-    sed -i "5i # Versi: $versi" /home/bebasid/public_html/hosts
-    sed -i "6i # Tanggal diperbarui: $sekarang" /home/bebasid/public_html/hosts
-    sed -i "7i # Total domain: ~$total" /home/bebasid/public_html/hosts
-    sed -i "8i # Mengambil data terbaru dari: https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts" /home/bebasid/public_html/hosts
-    sed -i "9i # Data diambil otomatis 4x sehari tiap 6 jam sekali" /home/bebasid/public_html/hosts
+    sed -i "4i \ " "$path/public_html/hosts"
+    sed -i "5i # Versi: $versi" "$path/public_html/hosts"
+    sed -i "6i # Tanggal diperbarui: $sekarang" "$path/public_html/hosts"
+    sed -i "7i # Total domain: ~$total" "$path/public_html/hosts"
+    sed -i "8i # Mengambil data terbaru dari: https://raw.githubusercontent.com/bebasid/bebasid/master/releases/hosts" "$path/public_html/hosts"
+    sed -i "9i # Data diambil dan divalidasi secara real-time" "$path/public_html/hosts"
 fi
